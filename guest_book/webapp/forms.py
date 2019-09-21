37e5120b7a66c53c935f1cc8982dbbadee258785
akyl <akyl.aydarbekov@gmail.com>
2019-09-21 18:41:26 +0600

    updates

--- guest_book/webapp/forms.py
+++ guest_book/webapp/forms.py
@@ -5,3 +5,4 @@ class NoteForm(forms.Form):
     name = forms.CharField(max_length=100, required=True, label='Имя: ')
     mail = forms.EmailField(required=True, label='Email: ')
     text = forms.CharField(max_length= 2000, required=True, label='Текст', widget=forms.Textarea)
+
