fee5976e41b14d869ed868e7c92e9c0a32fb9cb5
akyl <akyl.aydarbekov@gmail.com>
2019-09-21 12:27:55 +0600

    added model, data

--- guest_book/webapp/models.py
+++ guest_book/webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+NOTE_CATEGORY_CHOICES = (
+    ('active', 'Активно'),
+    ('blocked', 'Заблокировано'),
+)
+
+class Note(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя автора')
+    mail = models.EmailField(verbose_name='Почта автора')
+    text = models.TextField(max_length=2000, verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время создания')
+    category = models.CharField(max_length=20, verbose_name='Категория',
+                                choices=NOTE_CATEGORY_CHOICES, default=NOTE_CATEGORY_CHOICES[0][0])
+
+    def __str__(self):
+        return self.name
